f23055931fde45277f7468935215ff765f361f32
MyoMyoCU <septembermyo96@gmail.com>
2018-06-25 14:42:45 +0630

    list exercise

---
+++ ex34.py
@@ -0,0 +1,15 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+python = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
+ 
+print(f"the first animal is at 0 and is a {bear}")
+print(f"The third animal is at 2 and is a {peacock} ")
+print(f"The fist animals is {bear}")
+print(f"the animals at 3 is {peacock}")
+print(f"the fifth animal is {whale}")
+print(f"the sixth animal is {platypus}")
+
