8787fb62ff400390096ea943ce123658e8efbc2a
MyoMyoCU <septembermyo96@gmail.com>
2018-06-25 15:06:12 +0630

    class exercise

---
+++ ex40a.py
@@ -0,0 +1,35 @@
+import mystuff
+mystuff.apple()
+print(mystuff.tangerine)
+
+mystuff.apple()
+mystuff.tangerine
+
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = " And now a thousand of years between"
+
+    def apple(self):
+        print(" I am classy apple")
+
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+#mystuff['apple']
+
+mystuff.apple()
+print(mystuff.tangerine)
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
